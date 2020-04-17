aa4d79c5c774b284aa37f3533fe7e1b9fc1dfdd4
Marvin <marvin.polette@gmail.com>
2020-04-17 17:37:19 +0200

    fix deplacements

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,6 +6,9 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float _speed = 200f;
 
+    [SerializeField]
+    private float _jumpForce = 300;
+
     private Rigidbody2D _rb;
     private Vector3 _velocity = Vector3.zero;
     private PlayerControls _controls;
@@ -15,6 +18,7 @@ public class Character : MonoBehaviour
     {
         _controls = new PlayerControls();
         _controls.Gameplay.Move.performed += HandleMove;
+        _controls.Gameplay.Move.canceled += StopMove;
     }
 
     private void Start()
@@ -32,10 +36,21 @@ public class Character : MonoBehaviour
         _moveAxis = context.ReadValue<Vector2>();
     }
 
+    private void StopMove(InputAction.CallbackContext context)
+    {
+        _moveAxis = Vector2.zero;
+    }
+
     private void MoveCharacter()
     {
         Vector3 targetVelocity = new Vector2(_moveAxis.x * Time.fixedDeltaTime * _speed, _rb.velocity.y);
         _rb.velocity = Vector3.SmoothDamp(_rb.velocity, targetVelocity, ref _velocity, .01f);
+    }
+
+
+    private void Jump()
+    {
+        _rb.AddForce(new Vector2(0f, _jumpForce));
     }
 
     private void OnEnable()
