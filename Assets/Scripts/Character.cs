cd1a022111a0432133dd6e2095dc2217022df80f
Marvin <marvin.polette@gmail.com>
2020-04-17 17:14:02 +0200

    add movements

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,29 +3,39 @@ using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 200f;
+
+    private Rigidbody2D _rb;
+    private Vector3 _velocity = Vector3.zero;
     private PlayerControls _controls;
     private Vector2 _moveAxis;
-
-    [SerializeField]
-    private float _speed = 8f;
 
     private void Awake()
     {
         _controls = new PlayerControls();
-
         _controls.Gameplay.Move.performed += HandleMove;
     }
 
-    private void Update()
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
     }
 
     private void HandleMove(InputAction.CallbackContext context)
     {
         _moveAxis = context.ReadValue<Vector2>();
-        Debug.Log(_moveAxis);
+    }
 
+    private void MoveCharacter()
+    {
+        Vector3 targetVelocity = new Vector2(_moveAxis.x * Time.fixedDeltaTime * _speed, _rb.velocity.y);
+        _rb.velocity = Vector3.SmoothDamp(_rb.velocity, targetVelocity, ref _velocity, .01f);
     }
 
     private void OnEnable()
