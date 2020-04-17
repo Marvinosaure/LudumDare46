f527782eaa546bbf003e3a10c5b1eb22c45e071d
Corrado LONGONI <corradolongoni@Corrados-MacBook-Pro.local>
2020-04-17 22:05:23 +0200

    prefab burshes

---
+++ Assets/Scripts/Tiles/Tests/TileTestBlinkingBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileTestBlinkingBehaviour : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+
+
+    Vector3Int tilePosition;
+    Tile tile;
+    Tilemap tilemap;
+
+    private void Start()
+    {
+        var tmgo = GameObject.Find("BlinkingTilemap");
+        if (tmgo != null) tilemap = tmgo.GetComponent<Tilemap>();
+    }
+
+    void Update()
+    {
+        sprite.color = (Time.time % 1) * Color.white;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (tilemap != null) tilemap.SetTile(tilePosition, null);
+        Destroy(gameObject);
+    }
+
+    public void SetTile(Vector3Int pos, Tile t)
+    {
+        Debug.Log($"SetTile {pos} {t}");
+        tilePosition = pos;
+        tile = t;
+    }
+}
---
+++ Assets/Scripts/Tiles/Tests/TileTestBlinkingTile.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TileTestBlinkingTile : Tile
+{
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        Debug.Log($"StartUp {position} {tilemap} {go.GetComponent<TileTestBlinkingBehaviour>()}");
+
+        go.GetComponent<TileTestBlinkingBehaviour>().SetTile(position, this);
+
+        return true;
+    }
+
+    public override void RefreshTile(Vector3Int location, ITilemap tilemap)
+    {
+        Debug.Log($"Refresh Tile {location} {tilemap}");
+       
+    }
+
+    public override void GetTileData(Vector3Int location, ITilemap tilemap, ref TileData tileData)
+    {
+        Debug.Log($"GetTileData");
+        tileData.sprite = this.sprite;
+        tileData.color = this.color;
+        tileData.transform = this.transform;
+        tileData.gameObject = this.gameObject;
+        tileData.flags = this.flags;
+
+        tileData.colliderType = this.colliderType;
+    }
+
+#if UNITY_EDITOR
+    // The following is a helper that adds a menu item to create a RoadTile Asset
+    [MenuItem("Assets/Create/TileTestBlinkingTile")]
+    public static void CreateRoadTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Blinking Tile", "New Blinking Tile", "Asset", "Save Blinking Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<TileTestBlinkingTile>(), path);
+    }
+#endif
+}
