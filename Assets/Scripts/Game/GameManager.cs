cac3152cabb99af6f3afbf556bdbc96da75b7188
Marvin <marvin.polette@gmail.com>
2020-04-18 10:04:43 +0200

    update

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    private void Singleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
