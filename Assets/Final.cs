fbe61191fa0111428a4889bd7fa93a3e89bf7cf9
Marvin <marvin.polette@gmail.com>
2020-04-21 02:47:51 +0200

    add final

---
+++ Assets/Final.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Final : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Destroy(other.gameObject);
+
+        //gameObject.GetComponent<Animator>().SetBool("Final", true);
+        gameObject.transform.Find("WithBaby").GetComponent<Animator>().SetBool("Final", true);
+    }
+}
